003cd7af6eed184399afc799d5b1f7ed60176f6d
sparklestish <richardneaverscoy@gmail.com>
2019-05-23 13:56:58 +0000

    j
    f

---
+++ app/src/main/java/com/material/components/helper/URLs.java
@@ -0,0 +1,12 @@
+package com.material.components.helper;
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://142.4.5.189/waec/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+    public static final String URL_ACTIVATE= ROOT_URL + "activate";
+    public static final String URL_WAEC_CARD= ROOT_URL + "waec_cards";
+    public static final String URL_PLACEMENT_CARD= ROOT_URL + "placement_cards";
+}
